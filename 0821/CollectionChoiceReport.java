9f44f4a6970a023f05cf3055f549ff33ecd94beb
eunice910228 <eunice910228@gmail.com>
2026-08-25 20:23:02 +0800

    0821_homework_4

--- 0821/CollectionChoiceReport.java
+++ 0821/CollectionChoiceReport.java
@@ -1,4 +1,6 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -16,7 +18,7 @@ public class CollectionChoiceReport {
         searchHistory.add("generic");
         searchHistory.add("java");
         System.out.println("全部紀錄 = " + searchHistory);
-        System.out.println("size = " + searchHistory.size() + "含重複");
+        System.out.println("size = " + searchHistory.size());
         System.out.println("最近一次 = " + searchHistory.get(searchHistory.size() - 1));
         System.out.println("需求2：保存不重複會員編號");
         Set<String> members = new HashSet<>();
@@ -24,7 +26,7 @@ public class CollectionChoiceReport {
         for (String id : incoming) {
             System.out.println("add(" + id + ") = " + members.add(id));
         }
-        System.out.println("輸入" + incoming.length + "筆，實際保存 " + members.size() + "筆");
+        System.out.println("輸入" + incoming.length + "筆，實際保存" + members.size() + "筆");
         System.out.println("contains(A03) = " + members.contains("A03"));
         System.out.println("需求3：以學號查詢成績");
         Map<String, Integer> scores = new HashMap<>();
@@ -35,5 +37,27 @@ public class CollectionChoiceReport {
         System.out.println("get(S33) = " + scores.get("S33"));
         System.out.println("getOrDefault(S33, 0) = " + scores.getOrDefault("S33", 0));
         System.out.println("put(S01, 95) 舊值 = " + scores.put("S01", 95));
+		
+        System.out.println("需求4：依到達順序處理列印工作");
+        Deque<String> printQueue = new ArrayDeque<>();
+        printQueue.offerLast("報告.pdf");
+        printQueue.offerLast("履歷.docx");
+        printQueue.offerLast("海報.png");
+        System.out.println("佇列 = " + printQueue);
+        System.out.println("peekFirst() = " + printQueue.peekFirst());
+        while (!printQueue.isEmpty()) {
+            System.out.println("  列印 " + printQueue.pollFirst() + " 剩 " + printQueue);
+        }
+        System.out.println("空佇列 pollFirst() = " + printQueue.pollFirst());
+        System.out.println("需求5：復原最近操作");
+        Deque<String> undoStack = new ArrayDeque<>();
+        undoStack.push("新增文字");
+        undoStack.push("設定粗體");
+        undoStack.push("插入圖片");
+        undoStack.push("調整行距");
+        System.out.println("stack = " + undoStack);
+        System.out.println("peek() = " + undoStack.peek());
+        System.out.println("undo 撤銷 " + undoStack.pollFirst() + " 剩 " + undoStack);
+        System.out.println("undo 撤銷 " + undoStack.pollFirst() + " 剩 " + undoStack);
     }
 }
