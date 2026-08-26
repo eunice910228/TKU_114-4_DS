746340e79b571c363a955755aa323bec87e5c177
eunice910228 <eunice910228@gmail.com>
2026-08-26 11:47:22 +0800

    0824_homework_5

--- 0824/OrganizationTreeReport.java
+++ 0824/OrganizationTreeReport.java
@@ -1,13 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class OrgNode {
     String name;
     OrgNode left;
     OrgNode right;
+
     OrgNode(String name) {
         this.name = name;
     }
 }
 
 public class OrganizationTreeReport {
+
     static String findParent(OrgNode node, String target) {
         if (node == null) {
             return null;
@@ -24,6 +29,7 @@ public class OrganizationTreeReport {
         }
         return findParent(node.right, target);
     }
+
     static int findDepth(OrgNode node, String target) {
         if (node == null) {
             return -1;
@@ -41,6 +47,28 @@ public class OrganizationTreeReport {
         }
         return -1;
     }
+
+    static List<String> pathFromRoot(OrgNode node, String target) {
+        List<String> path = new ArrayList<>();
+        build(node, target, path);
+        return path;
+    }
+
+    private static boolean build(OrgNode node, String target, List<String> path) {
+        if (node == null) {
+            return false;
+        }
+        path.add(node.name);
+        if (node.name.equals(target)) {
+            return true;
+        }
+        if (build(node.left, target, path) || build(node.right, target, path)) {
+            return true;
+        }
+        path.remove(path.size() - 1);
+        return false;
+    }
+
     public static void main(String[] args) {
         OrgNode root = new OrgNode("註冊組");
         root.left = new OrgNode("課務組");
@@ -49,11 +77,19 @@ public class OrganizationTreeReport {
         root.left.right = new OrgNode("體育組");
         root.right.left = new OrgNode("文書組");
         root.right.right = new OrgNode("招生組");
+
         System.out.println("findParent(體育組) =" + findParent(root, "體育組"));
         System.out.println("findParent(註冊組) =" + findParent(root, "註冊組"));
         System.out.println("findDepth(文書組) =" + findDepth(root, "文書組"));
-        System.out.println("findParent(出納組) =" + findParent(root, "出納組"));
-        System.out.println("findDepth(出納組) =" + findDepth(root, "出納組"));
-        System.out.println("empty tree findDepth =" + findDepth(null, "任何"));
+        System.out.println("pathFromRoot(招生組) =" + pathFromRoot(root, "招生組"));
+
+        System.out.println("找不到的單位");
+        System.out.println("  findParent(學務處) =" + findParent(root, "學務處"));
+        System.out.println("  findDepth(學務處) =" + findDepth(root, "學務處"));
+        System.out.println("  pathFromRoot(學務處) =" + pathFromRoot(root, "學務處"));
+
+        System.out.println("empty tree");
+        System.out.println("  findDepth =" + findDepth(null, "任何"));
+        System.out.println("  pathFromRoot =" + pathFromRoot(null, "任何"));
     }
 }
