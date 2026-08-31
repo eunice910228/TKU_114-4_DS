aabf63a713fbc900e2617a052b6246322baf4e84
eunice910228 <eunice910228@gmail.com>
2026-08-31 22:46:25 +0800

    0831_homework_3

--- 0831/ArrayMinHeap.java
+++ 0831/ArrayMinHeap.java
@@ -35,11 +35,43 @@ public class ArrayMinHeap {
         }
         return data[0];
     }
+    int remove() {
+        if (size == 0) {
+            return -1;
+        }
+        int min = data[0];
+        size--;
+        data[0] = data[size];
+        int index = 0;
+        while (true) {
+            int left = index * 2 + 1;
+            int right = index * 2 + 2;
+            int smaller = index;
+            if (left < size && data[left] < data[smaller]) {
+                smaller = left;
+            }
+            if (right < size && data[right] < data[smaller]) {
+                smaller = right;
+            }
+            if (smaller == index) {
+                break;
+            }
+            swap(index, smaller);
+            index = smaller;
+        }
+        return min;
+    }
     int size() {
         return size;
     }
+    boolean isEmpty() {
+        return size == 0;
+    }
     int capacity() {
         return data.length;
+    }
+    int[] snapshot() {
+        return Arrays.copyOf(data, size);
     }
     public static void main(String[] args) {
         ArrayMinHeap heap = new ArrayMinHeap(4);
@@ -55,6 +87,14 @@ public class ArrayMinHeap {
         System.out.println("加入" + responseTimes.length + "筆後 size=" + heap.size()
                 + "，容量=" + heap.capacity());
         System.out.println("最短回應時間=" + heap.peek());
+        System.out.println("snapshot=" + Arrays.toString(heap.snapshot()));
+        System.out.print("由小到大取出=");
+        while (!heap.isEmpty()) {
+            System.out.print(heap.remove() + " ");
+        }
         System.out.println();
+        System.out.println("空heap isEmpty=" + heap.isEmpty());
+        System.out.println("空heap peek=" + heap.peek());
+        System.out.println("空heap remove=" + heap.remove());
     }
 }
